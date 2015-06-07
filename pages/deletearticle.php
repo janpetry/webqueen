e7a7ef8fcbd07bab82bb2abe222e01e2e3883501
janpetry <petry.jan@outlook.de>
2015-06-07 13:34:04 +0200

    Create deletearticle.php

---
+++ pages/deletearticle.php
@@ -0,0 +1,32 @@
+<?php
+	if(isset($_SESSION['logged'])) {
+?>
+<?php 
+	if(isset($_POST['post7'])) {
+		$system->deleteArticle($_POST['selectedid']);
+	}
+?>
+<div class="page-header">
+  <h1>Artikel löschen</h1>
+</div>
+<div class="alert alert-warning alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  <strong>Achtung!</strong> Achte auf die Groß- und Kleinschreibung. Wurde ein Artikel einmal aus dem Dark Visa Article System gelöscht so kann er nicht wiederhergestellt werden!
+</div>
+	<form method="post">
+        <div class="input-group">
+          <span class="input-group-addon"><i class="fa fa-trash fa-fw"></i></span>
+          <input type="text" name="selectedid" class="form-control" placeholder="Name des zu löschenden Artikels">
+        </div>
+		<br /><br />
+		<input type="submit" value="Löschen" class="btn btn-success" />
+		<input type="hidden" name="post7" />
+	</form>
+	<br />
+	<i class="fa fa-dashboard fa-1" style="padding-right: 3px;"></i> <a href="?site=admin&start">Zur&uuml;ck zum Dashboard</a>
+<?php
+	} else {
+		echo $system->errors[] = ERROR_ACCESS_DENIED;
+		print '<script type="text/javascript">setTimeout(function(){location.href="index.php";}, 1500);</script>';
+	}
+?>

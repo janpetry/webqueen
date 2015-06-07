0aecf463ee63ba5fcfc5032f570dfc1ac5d1fee1
janpetry <petry.jan@outlook.de>
2015-06-07 13:36:07 +0200

    Create jobs.php

---
+++ pages/jobs.php
@@ -0,0 +1,22 @@
+<div class="page-header">
+  <h1>Der Jugendfeuerwehr beitreten</h1>
+</div>
+<b>Was ist die Jugendfeuerwehr Bilsdorf?</b>
+<br />
+Die Jugendfeuerwehr Bilsdorf ist die Jugendorganisation der Freiwilligen Feuerwehr Nalbach, L&ouml;schbezirk Bilsdorf.
+<br />
+<br />
+<b>Wer kann Mitglied werden?</b><br /> 
+In die Jugendfeuerwehr Bilsdorf k&ouml;nnen Mädchen und Jungen die zwischen 8-16 Jahren sind aufgenommen werden.
+<br />
+<br />
+<b>Wie kann man sich anmelden?</b><br /> 
+Vor der Anmeldung sollten sich die Jugendlichen erst einmal einen Eindruck von dem machen, was sie später in der Jugendfeuerwehr erwartet. Aus diesem Grund besteht die M&ouml;glichkeit sich die &uuml;bungsdienste  1-2  Mal anzuschauen, bevor man sich anmeldet. Die eigentliche Aufnahme in die Jugendfeuerwehrabteilung wird durch den Jugendwart oder Jugendbetreuer vorgenommen. 
+<br />
+<br />
+<b>Wie sind die Jugendlichen versichert?</b><br />
+Versicherungsschutz besteht f&uuml;r die Jugendlichen während den Jugendfeuerwehrveranstaltungen, sowie auf dem direkten Weg von der Wohnung zur Feuerwehr und wieder zur&uuml;ck.
+<br />
+<br />
+<b>Noch Fragen?</b><br />
+Dann schicken sie uns doch eine Email an <a href="mailto:info@jfwbilsdorf.de">info@jfwbilsdorf.de</a>

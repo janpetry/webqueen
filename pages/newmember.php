bf8958d481900aa4d0cb5b54fd7981be08d180d6
janpetry <petry.jan@outlook.de>
2015-06-07 13:37:47 +0200

    Create newmember.php

---
+++ pages/newmember.php
@@ -0,0 +1,44 @@
+<?php
+	if(isset($_SESSION['logged'])) {
+?>
+<?php 
+	if(isset($_POST['post1'])) {
+		$firefighters->addYouthFirefighterMember($_POST['username'], $_POST['rang']);
+	}
+?>
+<div class="page-header">
+  <h1>Jugendmitglied hinzufügen</h1>
+</div>
+	<form method="post">
+        <div class="input-group">
+          <span class="input-group-addon"><i class="fa fa-user fa-fw"></i></span>
+          <input type="text" name="username" class="form-control" placeholder="Name">
+        </div>
+		<br />
+		<div class="input-group">
+			<span class="input-group-addon"><i class="fa fa-bookmark fa-fw"></i></span>
+			<input type="text" name="rang" class="form-control" placeholder="Dienstgrad" />
+        </div>
+		<br /><br />
+<?php
+	$i = 1;
+	
+	foreach($firefighters->getAllBadges() as $badge) {
+		echo '<input type="checkbox" name="badge_check_'.$i.'" />
+			  <img src="'.$badge['link'].'" alt="'.$badge['name'].'" data-toggle="tooltip" data-placement="top" title="'.$badge['name'].'" />
+			 <input type="hidden" name="sum" readonly="readonly" value="'.$i++.'" /> ';
+	}
+?>
+		<br /><br />
+		<input type="submit" value="Speichern" class="btn btn-success" />
+		<input type="hidden" name="post1" />
+	</form>
+	<br />
+	<i class="fa fa-dashboard fa-1" style="padding-right: 3px;"></i> <a href="?site=admin&start">Zur&uuml;ck zum Dashboard</a>
+<?php
+	} else {
+		echo $system->errors[] = ERROR_ACCESS_DENIED;
+		print '<script type="text/javascript">setTimeout(function(){location.href="index.php";}, 1500);</script>';
+	}
+?>
+			

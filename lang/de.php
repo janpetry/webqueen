3c6bb4f3b9842b77f826bb465f0b0b3a525e2bec
janpetry <petry.jan@outlook.de>
2015-06-07 13:40:28 +0200

    Create de.php

---
+++ lang/de.php
@@ -0,0 +1,22 @@
+<?php
+	// Messages
+	define('MESSAGE_LOGGED_IN', '<div class="alert alert-success" role="alert">Erfolgreich autorisiert! Bitte warten sie einen Augenblick ...</div>');
+	define('MESSAGE_LOGGED_OUT', '<div class="alert alert-success" role="alert">Sie haben sich erfolgreich abgemeldet.</div>');
+	define('MESSAGE_ADMIN_REGISTRATION', '<div class="alert alert-success" role="alert">Sie haben soeben einen weiteren Administrator registriert.</div>');
+	define('MESSAGE_ADMIN_DELETED', '<div class="alert alert-success" role="alert">Sie haben soeben einen Administrator aus der Datenbank gelöscht.</div>');
+	define('MESSAGE_ADD_ARTICLE', '<div class="alert alert-success" role="alert">Sie haben soeben einen neuen Artikel verfasst und online gestellt.</div>');
+	define('MESSAGE_DELETE_ARTICLE', '<div class="alert alert-success" role="alert">Sie haben soeben einen Artikel aus dem Archiv gelöscht.</div>');
+	define('MESSAGE_ADD_YOUTHFIREFIGHTER', '<div class="alert alert-success" role="alert">Sie haben ein Jugendfeuerwehrmitglied hinzugefügt.</div>');
+	define('MESSAGE_DELETE_YOUTHFIREFIGHTER', '<div class="alert alert-success" role="alert">Sie haben sein Jugendfeuerwehrmitglied gelöscht.</div>');
+	define('MESSAGE_ADD_YOUTHLEADER', '<div class="alert alert-success" role="alert">Sie haben einen Betreuer hinzugefügt.</div>');
+	define('MESSAGE_DELETE_YOUTHLEADER', '<div class="alert alert-success" role="alert">Sie haben einen Betreuer gelöscht.</div>');
+	
+	// Errors
+	define('ERROR_DATABASE_CONNECTION', '<div class="alert alert-warning" role="alert">Datenbank Verbindungsfehler! Stellen sie PHP Fehler Meldungen an, wenn sie dies noch nicht gemacht haben sollten.</div>');
+	define('ERROR_LOCKED', '<div class="alert alert-danger" role="alert">Sie wurden von einem Administrator dieser Seite gesperrt!</div>');
+	define('ERROR_PASSWORD_USERNAME_INCORRECT', '<div class="alert alert-danger" role="alert">Der Benutzer exisitiert nicht oder das eingegebene Passwort stimmt nicht mit dem angegebenem Username überein.</div>');
+	define('ERROR_ACCESS_DENIED', '<div class="alert alert-danger" role="alert">Dieser Bereich ist gesichert! Bitte logge dich zuerst ein.</div>');
+
+	// Wordings
+	#following... Just in progress :P
+	

760e40e9e692bcb86b65b5f0390aa2e01f8435ef
janpetry <petry.jan@outlook.de>
2015-06-07 13:35:51 +0200

    Create imprint.php

---
+++ pages/imprint.php
@@ -0,0 +1,49 @@
+<div class="page-header">
+  <h1>Impressum <small>Angaben gem&auml;&szlig; &sect; 5 TMG:</small></h1>
+</div>
+Diensteanbieter dieses Internetangebots im Sinne von Paragraf 5 Telemediengesetz (TMG) ist die Jugendfeuerwehr Bilsdorf, vertreten durch:
+<br><br>
+<b><?php echo $config['web_owner_name']; ?></b><br />
+<?php echo $config['web_owner_street']; ?><br />
+<?php echo $config['web_owner_residence']; ?><br />
+<h2>Kontakt:</h2>
+<table>
+	<tr>
+    	<td><i class="fa fa-phone fa-1" style="padding-right: 2px;"></i></td>
+        <td> <?php echo $config['web_owner_tel']; ?></td>
+    </tr>
+    <tr>
+    	<td><i class="fa fa-fax fa-1" style="padding-right: 2px;"></i></td>
+        <td> <?php echo $config['web_owner_fax']; ?></td>
+	</tr>
+    <tr>
+    	<td><i class="fa fa-send fa-1" style="padding-right: 2px;"></i></td>
+        <td><a href="mailto:info@jfwbilsdorf.de"><?php echo $config['web_owner_mail']; ?></a></td>
+    </tr>
+    <tr>
+		<td><i class="fa fa-mail-forward fa-1" style="padding-right: 2px;"></i></td>
+        <td><a href="index.php?site=contact">Kontaktformular</a></td>
+    </tr>
+</table>
+<h2>Informationen:</h2>
+<i class="fa fa-database fa-1" style="padding-right: 2px;"></i>&nbsp;
+<b>Verantwortlicher f&uuml;r Inhalte:</b><br />
+&raquo; Jugendfeuerwehr Bilsdorf
+<br />
+<br />
+<i class="fa fa-database fa-1" style="padding-right: 2px;"></i>&nbsp;
+<b>Technische Angaben:</b><br />
+&raquo; Technisch Verantwortlicher: Jan Petry ( <a href="mailto:darkvisa@janpetry.com">darkvisa@janpetry.com</a> )
+<br />
+<br />
+<i class="fa fa-gavel fa-1" style="padding-right: 2px;"></i>&nbsp;
+<b>Weitere Informationen:</b>
+<br />
+&raquo; <a href="?site=disclaimer">Haftungsausschluss (Disclaimer) anzeigen</a><br />
+<br />
+<div class="alert alert-info alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        Zum Zeitpunkt der Erstellung unserer Webseite, wurden alle Links zu externen Seiten Dritter ordnungsgem&uuml;&szlig; gepr&uuml;ft und f&uuml;r in Ordnung befunden.<br /> 
+        Dennoch können wir ihnen nicht versichern, dass die Inhalte nicht im Nachhinein von deren Betreibern ver&auml;ndert oder manipuliert wurden. <br />
+        Hierf&uuml;r &uuml;bernehmen wir keinerlei Haftung, wie sie bereits dem <a href="index.php?site=disclaimer" class="alert-link">Haftungsausschluss</a> entnehmen k&ouml;nnen.
+</div>

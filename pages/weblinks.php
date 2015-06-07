7718d5b9b25aaf757c723c9f9058345d14a6dc78
janpetry <petry.jan@outlook.de>
2015-06-07 13:38:39 +0200

    Create weblinks.php

---
+++ pages/weblinks.php
@@ -0,0 +1,30 @@
+<div class="page-header">
+  <h1>Weblinks</h1>
+</div>
+<img src="img/weblinks.png" style="float: left;" alt="weblinks icon" />
+<br />
+Wir sind st&auml;ndig bem&uuml;ht interessante und informative Internetseiten rund um das Thema Feuerwehr und Sicherheit im WWW zu erkunden. Hier haben wir f&uuml;r Sie eine Auswahl von Links bereitgestellt.
+<br />
+<br />
+<h3>Beh&ouml;rden, Organisationen und Sonstige Intressanten Seiten</h3>
+<br />
+<img src="img/links.png" style="float: left;" alt="links icon" /> <a href="http://lbz-bilsdorf.feuerwehr-nalbach.de/">LBZ. Bilsdorf Feuerwehr Nalbach</a>
+<br /><br />
+<img src="img/links.png" style="float: left;" alt="links icon" />  <a href="http://www.saarland.de/polizei.htm">Polizei Saarland</a>
+<br /><br />
+<img src="img/links.png" style="float: left;" alt="links icon" />  <a href="http://www.zrf-saar.de/">Zweckverband f&uuml;r Rettungsdienst und Feuerwehralarmierung</a>
+<br /><br />
+<img src="img/links.png" style="float: left;" alt="links icon" />  <a href="http://www.lfws.saarland.de/">Feuerwehrschule des Saarlandes</a>
+<br /><br />
+<img src="img/links.png" style="float: left;" alt="links icon" />  <a href="http://www.feuerwehr.info/">Feuerwehr.info</a>
+<br /><br />
+<img src="img/links.png" style="float: left;" alt="links icon" />  <a href="http://www.jugendfeuerwehr.info">Jugendfeuerwehr.info</a>
+<br /><br />
+<img src="img/links.png" style="float: left;" alt="links icon" />  <a href="http://www.nalbach.de/">Gemeinde Nalbach</a>
+<br /><br />
+<img src="img/links.png" style="float: left;" alt="links icon" />  <a href="http://lfv-saarland.de/">Landesfeuerwehrverband</a>
+<br /><br />
+<img src="img/links.png" style="float: left;" alt="links icon" />  <a href="http://www.dfv.org/home.html">Deutscher Feuerwehrverband</a>
+<br /><br />
+<img src="img/links.png" style="float: left;" alt="links icon" />  <a href="http://www.bbk.bund.de/DE/Home/home_node.html">Bundesamt f&uuml;r Bev&ouml;lkerungsschutz</a>
+<br /><br />

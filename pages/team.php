66004d476d4ba07499c46defa96fa2c5723a710f
janpetry <petry.jan@outlook.de>
2015-06-07 13:38:15 +0200

    Create team.php

---
+++ pages/team.php
@@ -0,0 +1,12 @@
+<div class="page-header">
+  <h1>Jugend <small>Jugendfeuerwehrmänner &amp; Jugendfeuerwehrfrauen</h1>
+</div>
+<?php
+	$userData = $firefighters->listMembers();
+?>
+<div class="page-header">
+  <h1>Betreuer <small>Jugendwart &amp; Betreuer</small></h1>
+</div>
+<?php
+	$userData = $firefighters->listYouthLeaders();
+?>
